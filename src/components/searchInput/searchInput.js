c5e8e41c755ab5007299f5f97a32d723063fc680
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-08-19 14:27:18 +0300

    add search input in Android and Ios

---
+++ src/components/searchInput/searchInput.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import {} from 'react-native';
+import { styles } from './searchInputStyle';
+import { COLORS, textStyles } from './../../config/';
+import { date } from './.././../lib/';
+import Swiper from 'react-native-swiper';
+import { Container, Header, Item, Input, Icon, Button, Text } from 'native-base';
+
+export default class SearchInput extends Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			
+		}
+	}
+
+	search(text) {
+		let { search } = this.props;
+		search(text)
+	}
+
+	render() {
+		return (
+			<Header searchBar rounded>
+				<Item>
+					<Icon name="ios-search" />
+					<Input
+						placeholder="Search"
+						onChangeText={(text) => this.search(text)}
+					/>
+					<Icon name="ios-people" />
+				</Item>
+				{/*<Button transparent>
+					<Text>Search</Text>
+				</Button>*/}
+			</Header>
+		)
+	}
+}
--- src/views/home/home.js
+++ src/views/home/home.js
@@ -17,6 +17,7 @@ import { styles } from './homeStyle';
 import { COLORS, textStyles, config } from '../../config/';
 import { rawDataHelper } from '../../lib/index';
 import Post from '../../components/post/post';
+import SearchInput from '../../components/searchInput/searchInput';
 import NewPostModal from '../../modals/newPost/newPost';
 
 const posts = require('./../../data/posts');
@@ -29,6 +30,7 @@ export default class Home extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			allPosts: posts,
 			posts,
 			showNewPostModal: false,
 		}
@@ -56,22 +58,25 @@ export default class Home extends Component {
 		this.setState({ showNewPostModal: false });
 	}
 
-
+	/**
+	 * create new post
+	 * @param {*} postObject 
+	 */
 	createPost(postObject) {
-		let { posts } = this.state
+		let { allPosts, posts } = this.state
 		postObject.id = 5;
 		postObject.username = 'Mohamed Mamdouh';
 		postObject.avatar = 'http://lorempixel.com/150/150/people/';
 		postObject.images = ["http://lorempixel.com/500/500/nature/",];
 		//postObject.created_time = "2017-01-09T08:20:15+0000";
 
-		posts.push(postObject)
+		allPosts.push(postObject);
+		posts.push(postObject);
 		// posts.reverse()
 		// posts.unshift(postObject)
 		// posts.splice(0, 0, postObject);
-			this.setState({
-				posts,
-			})
+		this.updateAllPosts(allPosts)
+		this.updatePosts(posts)
 		this.closeNewPostModal()
 	}
 
@@ -81,25 +86,59 @@ export default class Home extends Component {
 	renderRow(rowData, sectionID, rowID, highlightRow) {
 		return <Post postObject={rowData} />
 	}
+
+	/**
+	 * search on posts by user name
+	 */
+	search(key) {
+		let { allPosts } = this.state;
+		key = key.toLowerCase();
+		let posts = _.filter(allPosts, function (postObject) {
+			let username = postObject.username.toLowerCase();
+			let isContain = _.includes(username, key);
+			return isContain;
+		});
+		this.updatePosts(posts)
+		console.log(key, posts)
+	}
+
+	/**
+	 * update posts state
+	 * @param {*} posts 
+	 */
+	updatePosts(posts) {
+		this.setState({
+			posts,
+		})
+	}
+
+	/**
+	 * update akkl posts state
+	 * @param {*} allPosts 
+	 */
+	updateAllPosts(allPosts) {
+		this.setState({
+			allPosts,
+		})
+	}
+	
 	render() {
 		let { showNewPostModal, posts } = this.state;
 		let postsDataSource = ds.cloneWithRows(posts);
 		return (
 			<View style={styles.container}>
 				<View style={styles.topBar}></View>
+				<SearchInput search={(text) => this.search(text)}/>
 				<ListView
+					ref={ref => this.scrollView = ref}
 					style={styles.posts}
 					removeClippedSubviews={false}
 					dataSource={postsDataSource}
 					renderRow={this.renderRow.bind(this)}
-					
-					ref={ref => this.listView = ref}
-					onLayout={event => {
-						this.listViewHeight = event.nativeEvent.layout.height
+					onContentSizeChange={(contentWidth, contentHeight) => {
+						this.scrollView.scrollToEnd({ animated: true });
 					}}
-					onContentSizeChange={() => {
-						this.listView.scrollTo({y: this.listView.getMetrics().contentLength - this.listViewHeight})
-					}}
+					enableEmptySections={true}
 				/>
 				<View style={styles.bottomBar}>
 					<TouchableOpacity onPress={() => this.openNewPostModal()}>
@@ -109,10 +148,8 @@ export default class Home extends Component {
 
 				<NewPostModal
 					showModal={showNewPostModal}
-					closeModal={() => { this.closeNewPostModal() }}
-					//createPost={() => { this.createPost(postObject) }}
-					//close={this.props.onClosed.bind(this)}
-					createPost={this.createPost.bind(this)}
+					closeModal={() => this.closeNewPostModal() }
+					createPost={(postObject) => this.createPost(postObject) }
 				/>
 			</View>
 		)
