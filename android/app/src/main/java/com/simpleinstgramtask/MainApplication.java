b88ccac3cd28096f04333c08b1c49f31acd096c4
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-08-19 14:35:20 +0300

    add and link image Picker library

--- android/app/src/main/java/com/simpleinstgramtask/MainApplication.java
+++ android/app/src/main/java/com/simpleinstgramtask/MainApplication.java
@@ -3,6 +3,7 @@ package com.simpleinstgramtask;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
             new VectorIconsPackage(),
             new RNI18nPackage()
       );
