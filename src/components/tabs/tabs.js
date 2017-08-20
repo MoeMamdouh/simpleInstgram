9617ae8f6b889762080ed4578cbe8d1726f70904
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-08-20 15:37:00 +0300

    add toastrs messages to tabs

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { } from 'react-native';
 import { styles } from './tabsStyle';
+import { nativeFunctions } from './../../lib/';
 import { Container, Header, Content, Footer, FooterTab, Button, Icon, Text } from 'native-base';
 
 export default class Tabs extends Component {
@@ -17,19 +18,19 @@ export default class Tabs extends Component {
 		return (
 			<Footer>
 				<FooterTab>
-					<Button>
+					<Button onPress={() => nativeFunctions.toast('will be added soon ')}>
 						<Icon name="home" />
 					</Button>
-					<Button>
+					<Button onPress={() => nativeFunctions.toast('will be added soon ')}>
 						<Icon name="search" />
 					</Button>
 					<Button active onPress={openNewPostModal}>
 						<Icon active name="add" />
 					</Button>
-					<Button>
+					<Button onPress={() => nativeFunctions.toast('will be added soon ')}>
 						<Icon name="heart" />
 					</Button>
-					<Button>
+					<Button onPress={() => nativeFunctions.toast('will be added soon ')}>
 						<Icon name="person" />
 					</Button>
 				</FooterTab>
