5a624aa7580eaa8226bff319593f1e07554e1d83
Mohamed Mamdouh <mohamed.mamdouh@mowesolutions.com>
2017-09-18 21:53:59 +0300

    add react-navigation library

---
+++ src/mainNavigator.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  AppRegistry,
+  Text,
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    return <Text>Hello, Navigation!</Text>;
+  }
+}
+
+export default const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+});
+
+// if you are using create-react-native-app you don't need this line
+AppRegistry.registerComponent('SimpleApp', () => SimpleApp);
